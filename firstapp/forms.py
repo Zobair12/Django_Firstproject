693ccad8bae01252af807fdfcd40ed0da56ed2f1
Zobair12 <alamjobayer5@gmail.com>
2025-06-03 21:09:45 +0600

    Forms Model

---
+++ firstapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms 
+from firstapp.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from firstapp.forms import ContactForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -11,8 +13,16 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
-    if request.methos == 'POST':
+    print(request.POST)
+    if request.method == 'POST':
         
         form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Thank for contacting with us.')
+            return redirect('/')
+        else:
+            messages.error(request,'Invalid date.')
+            return redirect('contact')
     
-    return render(request, 'contact.html')
+    return render(request,'contact.html')
