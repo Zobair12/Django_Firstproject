ad5a2b42785b51473e3fc3e42ee4c354dea6a1a7
Zobair12 <alamjobayer5@gmail.com>
2025-07-26 21:18:48 +0600

    Time zone Update

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Contact(models.Model):
@@ -7,8 +8,8 @@ class Contact(models.Model):
     email = models.EmailField()
     subject = models.CharField(max_length=255)
     message = models.TextField()
-    #created_at = models.DateTimeField(auto_now_add=True)
-    #updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'Contact'
